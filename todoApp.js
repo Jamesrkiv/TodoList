b2b9d51bde7463405857b93e7d3651ccd1da5aa8
James Kent <james@helmshomecare.com>
2024-04-11 10:41:11 -0400

    Started building out functions, started integrating electron

--- todoApp.js
+++ todoApp.js
@@ -1,6 +1,7 @@
-//
+var tracking_num = 0;
 
-function createListItem(text, desc, due, priority)
+// Creates a new item in the todo list
+function createListItem(text, desc, due, priority, idnum)
 {
 	var priorityHTML = "";
 	switch(priority)
@@ -17,8 +18,8 @@ function createListItem(text, desc, due, priority)
 		default:
 			break;
 	}
-	var item = '<li class="d-flex list-group-item p-3 overflow-hidden">'+
-					'<button type="button" class="inln btn btn-outline-success" style="height: 50px; width:50px">'+
+	var item = '<li class="d-flex list-group-item p-3 overflow-hidden" id="item'+idnum+'">'+
+					'<button type="button" class="inln btn btn-outline-success" style="height: 50px; width:50px" onclick="deleteListItem('+idnum+');">'+
 						'<svg xmlns="http://www.w3.org/2000/svg" width="25" height="34" fill="currentColor" class="bi bi-check btn-icon" viewBox="0 0 16 16">'+
 							'<path d="M10.97 4.97a.75.75 0 0 1 1.07 1.05l-3.99 4.99a.75.75 0 0 1-1.08.02L4.324 8.384a.75.75 0 1 1 1.06-1.06l2.094 2.093 3.473-4.425z"/>'+
 						'</svg>'+
@@ -30,10 +31,20 @@ function createListItem(text, desc, due, priority)
 					'</div>'+
 				'</li>';
 	var list = document.getElementById("todo-list");
-	list.innerHTML = item;
+	list.innerHTML += item;
 }
 
-window.onload = function ()
+// Deletes an existing item in the todo list
+function deleteListItem(idnum)
 {
-	createListItem("Test list item", "Test", "04/08/24", "high");
+	document.getElementById("item"+idnum).remove();
 }
+
+// Function to run when the form for creating a new todo list item is submitted
+function submitItemForm()
+{
+	createListItem("Test list item "+tracking_num, "Test", "04/08/24", "high", tracking_num);
+	tracking_num++;
+}
+
+console.log("Process started...");
