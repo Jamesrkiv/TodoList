38df14cb07ae29908f94d23e00f16ff1dd35ae78
James Kent <james@helmshomecare.com>
2024-04-15 15:45:31 -0400

    Added list sorting functions

--- todoApp.js
+++ todoApp.js
@@ -1,7 +1,6 @@
 // GLOBAL VARIABLES /////////////////////////////////////////////////////////////////////////////////////////
 
 var tracking_num = parseInt(Date.now());
-var todoData = "";
 
 // FUNCTIONS ////////////////////////////////////////////////////////////////////////////////////////////////
 
@@ -10,13 +9,13 @@ function createListItem(desc, due, priority, idnum) {
 	var priorityHTML = "";
 	var dueHTML = "";
 	switch(priority) {
-		case "Low":
+		case 1:
 			priorityHTML = '<span class="badge bg-info rounded-pill v-middle">Low</span>';
 			break;
-		case "Medium":
+		case 2:
 			priorityHTML = '<span class="badge bg-warning rounded-pill v-middle">Med</span>';
 			break;
-		case "High":
+		case 3:
 			priorityHTML = '<span class="badge bg-danger rounded-pill v-middle">High</span>';
 			break;
 		default:
@@ -50,15 +49,28 @@ function submitItemForm() {
 	var desc = document.getElementById("inputDesc");
 	var due = document.getElementById("dueDate");
 	var priority = document.getElementById("selectPriority");
+	var priorityv = 0; // It's easier to sort by a number
 	var descInput = document.getElementById("inputDesc");
-
+	switch(priority.value) {
+		case "High":
+			priorityv = 3;
+			break;
+		case "Medium":
+			priorityv = 2;
+			break;
+		case "Low":
+			priorityv = 1;
+			break;
+		default:
+			priorityv = 0;
+			break;
+	}
 	if (!descInput.checkValidity()) {
 		descInput.classList.add("is-invalid");
 		return;
 	}
-	
-	createListItem(desc.value, due.value, priority.value, tracking_num);
-	saveData(desc.value, due.value, priority.value, tracking_num);
+	createListItem(desc.value, due.value, priorityv, tracking_num);
+	saveData(desc.value, due.value, priorityv, tracking_num);
 	$("#item-modal").modal('hide');
 	tracking_num++;
 	desc.value = "";
@@ -92,11 +104,60 @@ function deleteData(idnum) {
 	window.electronAPI.delete(idnum);
 }
 
+// Sorts by ID number to put in order created
+async function defaultSort() {
+	document.getElementById("todo-list").innerHTML = "";
+	var data = await window.electronAPI.load();
+	data.sort(function(a,b) { 
+		return a.idn - b.idn
+	});
+	for (var i = 0; i < data.length; i++) {
+		createListItem(data[i]["txt"], data[i]["due"], data[i]["pri"], data[i]["idn"]);
+	}
+}
+
+// Sorts by priority, secondary sort by ID
+async function prioritySort() {
+	document.getElementById("todo-list").innerHTML = "";
+	var data = await window.electronAPI.load();
+	data.sort(function(a,b) {
+		if (b.pri > a.pri) return 1;
+		else if (b.pri < a.pri) return -1;
+		else {
+			if (a.idn > b.idn) return 1;
+			else if (a.idn < b.idn) return -1;
+			else return 0;
+		}
+	});
+	for (var i = 0; i < data.length; i++) {
+		createListItem(data[i]["txt"], data[i]["due"], data[i]["pri"], data[i]["idn"]);
+	}
+}
+
+// Sorts by due date, secondary sort by ID
+async function dateSort() {
+	document.getElementById("todo-list").innerHTML = "";
+	var data = await window.electronAPI.load();
+	data.sort(function(a,b) { 
+		if (a.due.trim() == "" && b.due.trim() != "") return 1; 
+		else if (a.due.trim() != "" && b.due.trim() == "") return -1;
+		else {
+			if (new Date(a.due) > new Date(b.due)) return 1;
+			else if (new Date(a.due) < new Date(b.due)) return -1;
+			else {
+				if (a.idn > b.idn) return 1;
+				else if (a.idn < b.idn) return -1;
+				else return 0;
+			}
+		}
+	});
+	for (var i = 0; i < data.length; i++) {
+		createListItem(data[i]["txt"], data[i]["due"], data[i]["pri"], data[i]["idn"]);
+	}
+}
+
 // Runs when webpage loaded
-window.onload = async (event) => {
-	// window.electronAPI.test("Init successful");
-	todoData = await window.electronAPI.load();
-	for (var i = 0; i < todoData.length; i++)
-		createListItem(todoData[i]["txt"], todoData[i]["due"], todoData[i]["pri"], todoData[i]["idn"]);
-};
+window.onload = (event) => {
+	defaultSort();
+}
 
