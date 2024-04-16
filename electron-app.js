aff4e2baf064a12928f24ede86fc25957627094f
James Kent <james@helmshomecare.com>
2024-04-16 09:57:06 -0400

    Enforced CSP

--- electron-app.js
+++ electron-app.js
@@ -17,6 +17,7 @@ const createWindow = () => {
 		icon: __dirname + '/icon.ico',
 		webPreferences: {
 			preload: path.join(__dirname, 'preload.js'),
+			nodeIntegration: false,
 			contextIsolation: true,
 		}
 	});
--- todoApp.js
+++ todoApp.js
@@ -24,7 +24,7 @@ function createListItem(desc, due, priority, idnum) {
 	}
 	if (due) dueHTML = '<span class="badge bg-secondary rounded-pill v-middle">'+due+'</span> ';
 	var item = '<li class="d-flex list-group-item p-3 overflow-hidden" id="item'+idnum+'">'+
-					'<button type="button" class="inln btn btn-outline-success" style="height: 50px; width:50px" onclick="deleteListItem('+idnum+');test();">'+
+					'<button type="button" class="inln btn btn-outline-success" style="height: 50px; width:50px" onclick="deleteListItem('+idnum+');">'+
 						'<svg xmlns="http://www.w3.org/2000/svg" width="25" height="34" fill="currentColor" class="bi bi-check btn-icon" viewBox="0 0 16 16">'+
 							'<path d="M10.97 4.97a.75.75 0 0 1 1.07 1.05l-3.99 4.99a.75.75 0 0 1-1.08.02L4.324 8.384a.75.75 0 1 1 1.06-1.06l2.094 2.093 3.473-4.425z"/>'+
 						'</svg>'+
